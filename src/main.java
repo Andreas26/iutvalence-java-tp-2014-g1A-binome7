bea5d3b12b7d622c792c38c8ab37c94acd1b23ad
Andréas Godicheau <andreas.godicheau@iut-valence.fr>
2014-02-24 16:16:04 +0100

    Début de séance 2

---
+++ src/main.java
@@ -0,0 +1,41 @@
+public class main
+{
+    public static void main (String[] args);
+    
+    {
+    private boolean piegee;
+    
+    private boolean marquee;
+    
+    private boolean ouverte;
+    
+    private int nbBombesAdj
+}
+
+    public void piegee()
+    {
+	this.piegee = true;
+    }
+    public void nonpiegee()
+    {
+	this.piegee = false;
+    }
+
+    public void marquee()
+    {
+	this.marquee = true;
+    }
+    public void nonmarquee()
+    {
+	this.marquee = false;
+    }
+    
+    public void ouverte()
+    {
+	this.ouverte = true;
+    }
+    public void fermee()
+    {
+	this.ouverte = false;
+    }
+}
