48b0dcc8cb205bb5b121e20c3463985c41bf08f5
Andréas Godicheau <andreas.godicheau@iut-valence.fr>
2014-02-24 17:29:01 +0100

    24/02/14

---
+++ src/Application.java
@@ -0,0 +1,9 @@
+public class Application
+{
+    public static void main (String[] args)
+    {
+	    Demineur partie = new Demineur();
+	    partie.jouer();
+    }
+    
+}
---
+++ src/Demineur.java
@@ -0,0 +1,17 @@
+
+public class Demineur
+{
+    private Case[][] plateau;
+    private int nbCoups;
+    private final static nbBombes;
+    private final static tailleMatrice;
+    
+    public Demineur()  
+    {     
+	                                    /* init plateau */
+    
+    
+                                             /* init nbCoups */
+    }
+
+}
