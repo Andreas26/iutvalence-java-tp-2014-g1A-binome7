7baab81146743845813fb16eeab59ec47bb47d5c
Andréas Godicheau <andreas.godicheau@iut-valence.fr>
2014-04-02 17:46:33 +0200

    Ajout de plusieurs méthodes : PlacerBombesAleatoirement determinerNombreBombesAdjacentes*
    Incrémentation de plusieurs variables.
    
    Ajout d'attributs et de méthodes dans la classe Case
    
    (si la case est minée, découverte, qu'elle présente un drapeau...)

--- src/Case.java
+++ src/Case.java
@@ -1,7 +1,49 @@
 public class Case
 {
-    private final boolean caseMine; //La case est soit minée, soit vide
-    private final boolean caseDecouverte; //La case est soit découverte, soit non découverte
-    private final boolean caseAvecDrapeau; //La case est marquée d'un drapeau, ou non
+    private boolean minee; // La case est soit min�e, soit vide
+    private boolean decouverte; // La case est soit d�couverte, soit non d�couverte
+    private boolean drapeau; // La case est marqu�e d'un drapeau, ou non
+    private int minesAdjacentes;
     
+    public Case() {
+	minee = false;
+	decouverte = false;
+	drapeau = false;
+	minesAdjacentes = 0;
+    }
+    
+    public boolean minee() {
+	return minee;
+    }
+    
+    public void miner() {
+	minee = true;
+    }
+    
+    public void decouvrir() {
+	decouverte = true;
+    }
+    
+    public void incrementerAdjacent() {
+	minesAdjacentes++;
+    }
+    
+    public int adjacents() {
+	return minesAdjacentes;
+    }
+    
+    @Override
+    public String toString() {
+	if (decouverte && !minee) {
+	    return ".";
+	}
+	if (decouverte && minee) {
+	    return "*";
+	}
+	/* XXX */
+	if (!decouverte) {
+	    return "#";
+	}
+	return "X";
+    }
 }
--- src/Demineur.java
+++ src/Demineur.java
@@ -1,69 +1,115 @@
-public class Demineur
-{
-    /** Définition des attributs */
-    
-    // Les compteurs
-    private int compteurDeCoups;
-    private int compteurDeCasesDecouvertes;
-    private int compteurDeDrapeaux;
-    
-    
-    // Les différents tableaux
-    private boolean[][] tableauPourCacher;
-    private boolean[][] tableauDesDrapeaux;
-    
-    // Définition de la taille du tableau et du nombre de mines
-    
-    private final static int NOMBRE_DE_LIGNES = 10;
-    private final static int NOMBRE_DE_COLONNES = 10;
-    private final static int NOMBRE_DE_MINES = 10;
-    
-    // Le plateau de jeu
-    
-    private Case[][] plateauDeJeu;
-      
-    private boolean estMinee;
-    private int nombreDeMinesAdjacentes;
-    
-   /** Fin des définitions des attributs */
-    /** la ligne suivante : public Demineur() est le constructeur*/
-    public Demineur()
-    {
-	this.compteurDeCoups = 0;
-	this.compteurDeCasesDecouvertes = 0;
-	this.compteurDeDrapeaux = 0;
-	this.nombreDeMinesAdjacentes = 0;
+import java.util.Arrays;
 
-	// On crée les 3 tableaux composant le démineur
+public class Demineur {
+	/** D�finition des attributs */
+
+	// Les compteurs
+	private int compteurDeCoups;
+	private int compteurDeCasesDecouvertes;
+	private int compteurDeDrapeaux;
+
+	// D�finition de la taille du tableau et du nombre de mines
+	private final static int NOMBRE_DE_LIGNES = 4;
+	private final static int NOMBRE_DE_COLONNES = 4;
+	private final static int NOMBRE_DE_MINES = 4;
+
+	// Le plateau de jeu
+	private Case[][] plateauDeJeu;
+
+	public Demineur() {
+		this.compteurDeCoups = 0;
+		this.compteurDeCasesDecouvertes = 0;
+		this.compteurDeDrapeaux = 0;
+
+		// On cr�e les 3 tableaux composant le d�mineur
+		this.plateauDeJeu = new Case[NOMBRE_DE_LIGNES][NOMBRE_DE_COLONNES];
+
+		// D�finir que toutes les cases ne sont pas min�es, par d�faut
+		for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
+			for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
+				plateauDeJeu[numeroDeLigne][numeroDeColonne] = new Case();
+	}
 	
-	this.plateauDeJeu = new Case[NOMBRE_DE_LIGNES][NOMBRE_DE_COLONNES];
-	this.tableauPourCacher = new boolean[NOMBRE_DE_LIGNES][NOMBRE_DE_COLONNES];
-	this.tableauDesDrapeaux = new boolean[NOMBRE_DE_LIGNES][NOMBRE_DE_COLONNES];
-	
-	for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
-	    for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
-		this.estMinee=false;
-		
-    }
-    
-    public void placerBombesAleatoirement()
+	public void jouer() {
+	    placerBombesAleatoirement();
+	    System.out.println(this);
+	}
+
+
+	// M�thode qui place des bombes de fa�on al�atoire sur l'ensemble du plateau
+	// de jeu
+
+	public void placerBombesAleatoirement()
     {
-	    for(int numeroDeLigne ; numeroDeLigne<nombreDeLignes ; numeroDeLigne++){
-	for(int numeroDeColonne ; numeroDeColonne<nombreDeColonnes ; numeroDeColonne++){
-	    Math.random()*100 < pourcent)
+	    int nbMines = 0;
+	    while (nbMines < NOMBRE_DE_MINES) {
+	    for(int numeroDeLigne=0 ; numeroDeLigne<NOMBRE_DE_LIGNES ; numeroDeLigne++){
+	    	for(int numeroDeColonne=0 ; numeroDeColonne<NOMBRE_DE_COLONNES ; numeroDeColonne++){
+	    	    if (Math.random()*100 < 10) {
+	    		plateauDeJeu[numeroDeLigne][numeroDeColonne].miner();
+	    		nbMines++;
+	    		if (nbMines == NOMBRE_DE_MINES) {
+	    		    return;
+	    		}
+	    	    }
 		}
+	    }
 	}
     }
 
-    public void decouvrirCase()
+	/** Calcul l'ensemble des nombres de bombes adjacentes des cases du plateau. */
+	public void determinerNombreDeBombesAdjacentes()
     {
+	for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++) {
+	    for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++) {
+		Case caseCourante = this.plateauDeJeu[numeroDeLigne][numeroDeColonne];
+		if (plateauDeJeu[numeroDeLigne-1][numeroDeColonne-1].minee()) {
+		    caseCourante.incrementerAdjacent();
+		}
+		
+//	XXX	
+//	if numeroDeLigne-1, numeroDeColonne
+//	if estMinee=true
+//	nombreDeMinesAdjacentes++;
+//	
+//	if numeroDeLigne-1, numeroDeColonne+1
+//	if estMinee=true
+//	nombreDeMinesAdjacentes++;
+//	
+//	if numeroDeLigne, numeroDeColonne-1
+//	if estMinee=true
+//	nombreDeMinesAdjacentes++;
+//	
+//	if numeroDeLigne, numeroDeColonne+1
+//	if estMinee=true
+//	nombreDeMinesAdjacentes++;
+//	
+//	if numeroDeLigne+1, numeroDeColonne-1
+//	if estMinee=true
+//	nombreDeMinesAdjacentes++;
+//	
+//	if numeroDeLigne+1, numeroDeColonne
+//	if estMinee=true
+//	nombreDeMinesAdjacentes++;
+//	
+//	if numeroDeLigne+1, numeroDeColonne+1
+//	if estMinee=true
+//	nombreDeMinesAdjacentes++;
+	    }
+	}
+	}
+
+	public String toString()
+	{
+	    /* XXX */
+		for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++) {
+			for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++) {
+				System.out.print(plateauDeJeu[numeroDeLigne][numeroDeColonne]);
+			}
+			System.out.print("\n");
+		}
+		return "";
+	}
 	
-    }
-    
-    public void determinerNombreDeBombesAdjacentes()
-    {
-	for (int numeroDeLigne = 0; numeroDeLigne < NOMBRE_DE_LIGNES; numeroDeLigne++)
-	    for (int numeroDeColonne = 0; numeroDeColonne < NOMBRE_DE_COLONNES; numeroDeColonne++)
-		if 
-    }
+	
 }
